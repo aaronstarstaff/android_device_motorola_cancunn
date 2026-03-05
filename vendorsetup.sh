f26b64020cf2e7238e3b521e4622cd8ce9b7d282
Metroid <159981647+aaronstarstaff@users.noreply.github.com>
2026-03-04 21:32:16 -0800

    Change lunch combo from omni to twrp for cancunn

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_cancunn-user
-add_lunch_combo omni_cancunn-userdebug
-add_lunch_combo omni_cancunn-eng
+add_lunch_combo twrp_cancunn-user
+add_lunch_combo twrp_cancunn-userdebug
+add_lunch_combo twrp_cancunn-eng
